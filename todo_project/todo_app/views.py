32e5af14a1dea8a9135a49784cf4eeb832993390
KayongoYongo <sknsammie10@gmail.com>
2024-01-31 00:44:16 +0000

    added the end points for listing and adding tasks

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
-# Create your views here.
+# Create your views here
+def task_list(request):
+    """
+    This function deals with rendering all tasks onto the html form
+    """
+    tasks = Task.objects.all()
+    return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    """
+    This function deals with adding tasks to the database
+    """
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        Task.objects.create(title=title)
+        return redirect('task_list')
+
+    return render(request, 'todo/add_task.html')
