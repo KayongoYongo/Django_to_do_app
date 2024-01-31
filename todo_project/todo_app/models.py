ea4de1d0b92790740cf7b47e36e08032bcdf0a59
KayongoYongo <sknsammie10@gmail.com>
2024-01-31 00:19:44 +0000

    created model and applied migrations

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# A model for a todo list
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
