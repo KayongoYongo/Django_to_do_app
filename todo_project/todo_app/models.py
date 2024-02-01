a5befbbd702f7133e6abf8c873eb8fc02566984d
KayongoYongo <sknsammie10@gmail.com>
2024-02-01 02:47:44 +0000

    updates to the database

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -4,5 +4,6 @@ from django.db import models
 
 # A model for a todo list
 class Task(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
