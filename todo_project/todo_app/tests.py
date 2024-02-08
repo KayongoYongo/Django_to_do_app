066e21141113ee27e3c4650af22f5cdab0534ef3
KayongoYongo <sknsammie10@gmail.com>
2024-02-08 18:17:22 +0000

    delete and update task

--- todo_project/todo_app/tests.py
+++ todo_project/todo_app/tests.py
@@ -15,3 +15,17 @@ class TaskModelTestCase(TestCase):
     def test_query_task(self):
         queried_task = Task.objects.get(title="Test Task")
         self.assertEqual(queried_task, self.task)
+
+    def test_update_task(self):
+        self.task.title = "Updated Task"
+        self.task.completed = True
+        self.task.save()
+        updated_task = Task.objects.get(pk=self.task.pk)
+        self.assertEqual(updated_task.title, "Updated Task")
+        self.assertTrue(updated_task.completed)
+
+    def test_delete_task(self):
+        task_count_before_delete = Task.objects.count()
+        self.task.delete()
+        task_count_after_delete = Task.objects.count()
+        self.assertEqual(task_count_after_delete, task_count_before_delete - 1)
