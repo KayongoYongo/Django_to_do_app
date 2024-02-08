6bcf3676fc53a548e86087405f6cdeded055d29e
KayongoYongo <sknsammie10@gmail.com>
2024-02-08 17:40:56 +0000

    create model test

--- todo_project/todo_app/tests.py
+++ todo_project/todo_app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Task
 
 # Create your tests here.
+
+# Models test
+class TaskModelTestCase(TestCase):
+    def setUp(self):
+        self.task = Task.objects.create(title="Test Task", completed=False)
+
+    def test_create_task(self):
+        self.assertEqual(self.task.title, "Test Task")
+        self.assertFalse(self.task.completed)
