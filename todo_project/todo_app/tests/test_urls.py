d875e7d185d55e1288cd45c4ecd3416d0865cfa7
KayongoYongo <sknsammie10@gmail.com>
2024-02-13 18:46:44 +0000

    test urls

--- todo_project/todo_app/tests/test_urls.py
+++ todo_project/todo_app/tests/test_urls.py
@@ -10,3 +10,18 @@ class URLsTestCase(TestCase):
     def test_add_task_url(self):
         response = self.client.get(reverse('add_task'))
         self.assertEqual(response.status_code, 200)
+
+    def test_update_task_url(self):
+        task_id = 1
+        response = self.client.get(reverse('update_task', args=[task_id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_task_url(self):
+        task_id = 1
+        response = self.client.get(reverse('delete_task', args=[task_id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_find_task_url(self):
+        task_id = 1
+        response = self.client.get(reverse('find_task', args=[task_id]))
+        self.assertEqual(response.status_code, 200)
